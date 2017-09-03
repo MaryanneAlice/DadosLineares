a5e3e7ac2c792e01267c22e74927c42ac6af2290
Maryanne Alice <maryannealice@live.com>
2017-09-03 10:50:29 -0300

    Create Principal.java

---
+++ primeiraPilha/Principal.java
@@ -0,0 +1,14 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Pilha p1 = new Pilha(5);
+        
+        p1.size();
+        p1.push(5);
+        p1.push("A");
+        
+               
+        p1.show();
+    }
+    
+}
